0c9deea529f1e0d26d8a9359659cfe9ae39d236b
Camila Martín <camilamartin1403@gmail.com>
2024-06-13 10:16:15 -0300

    delete comments

--- src/main/java/com/mycompany/cinema/Cinema.java
+++ src/main/java/com/mycompany/cinema/Cinema.java
@@ -1,6 +1,5 @@
 package com.mycompany.cinema;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Cinema {
@@ -12,6 +11,7 @@ public class Cinema {
         Scanner sc = new Scanner(System.in);
         int row, seat = 0;
         String answer;
+        String seeSeats;
         
         for (int r = 0; r < 10; r++) {
             for (int c = 0; c < 10; c++) {
@@ -23,9 +23,13 @@ public class Cinema {
         System.out.println("--------- WELCOME TO THE RESERVATION SYSTEM ---------");
         
         while(occupied!=true){
-            System.out.println("Do you want to see the free seats?");
+            System.out.println("Do you want to see the free seats? (Y= Yes, Another= No) ");
+            seeSeats = sc.next();
             
-            //llamo al metodo
+            if (seeSeats.equalsIgnoreCase("Y")) {
+                showSeats(seats);
+            }
+            
             
             System.out.println("Enter row and seat to reserve: \n");
             System.out.println("Row (0 to 9): \n");
@@ -45,7 +49,7 @@ public class Cinema {
             System.out.println("You want to finalize the reservation? (Y= Yes, Another= No) ");
             answer = sc.next();
             
-            if (answer.equalsIgnoreCase("S")) {
+            if (answer.equalsIgnoreCase("Y")) {
                 occupied= true;
             }
         }
@@ -63,80 +67,3 @@ public class Cinema {
         }
     }
 }
-
-        /*final char FREE = 'F';
-        
-        char [][] seats = new char[10][10];
-        fillSeats (seats, FREE);
-                
-        Scanner sc = new Scanner(System.in);
-        boolean executeMenu = true;
-        int option;
-
-        while(executeMenu){
-            showSeats(seats);
-            
-            System.out.println("1. Reserve a seat");
-            System.out.println("2. I'm done with my reservation");
-            
-            option = sc.nextInt();
-            
-            try {
-                System.out.println("Write one of the options");
-                option = sc.nextInt();
-                
-                switch (option) {
-                    case 1:
-                        
-                        System.out.println("Choose a ROW from 0 to 9 to reserve");
-                        int seatRow = sc.nextInt();
-                        
-                        System.out.println("Choose a COLUMN from 0 to 9 to reserve");
-                        int seatColumn = sc.nextInt();
-                        
-                        if (withinLimitOfArray(seats.length, seats[0].length, seatRow, seatColumn)) {
-                            if (seats[seatRow][seatColumn] == FREE) {
-                                seats[seatRow][seatColumn] = 'O';
-                                System.out.println("Seat reserved successfully!");
-                            } else {
-                                System.out.println("Seat already occupied. Please choose another seat.");
-                            }
-                        }
-                            
-                        break;
-
-                    case 2:
-                        System.out.println("See you at the next reservation! :)");
-                        executeMenu = false;
-                        break;
-                    
-                    default:
-                        System.out.println("Only option 1 or 2 is allowed");
-                }
-                
-            }catch (InputMismatchException e){
-                System.out.println("Invalid input. Please enter a number.");
-                sc.nextLine();
-            }
-        }
-    }
-    
-    
-                System.out.println("");
-            }
-    }
-    
-    public static void fillSeats(char[][] seats, char symbol){
-        for (int i = 0; i < seats.length; i++) {
-            for (int j = 0; j < seats[0].length; j++) {
-                seats[i][j] = symbol;
-            }
-        }
-    }
-    
-    public static boolean withinLimitOfArray(int rowLength, int columnLength, int x, int y) {
-        return x>= 0 && x< rowLength && y>=0 && y <columnLength;
-    }
-    */
-        
-    
